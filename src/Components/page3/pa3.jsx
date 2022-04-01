b56791b109e855591a74f209e12137781c8cda19
nHari9 <nharikishorereddy9@gmail.com>
2022-04-01 17:06:44 +0530

    page

---
+++ src/Components/page3/pa3.jsx
@@ -0,0 +1,358 @@
+import React from "react";
+import Styles from "./pa3.css";
+
+const Page3 = () => {
+  return (
+    <div className="page3">
+      <div className="p3head">
+        <div className="p3div">
+          <div className="p3div1">
+            <span className="p3span">Class 1 math (India)</span>
+            <h1 className="p3h1">Unit: Numbers from 1 to 9</h1>
+          </div>
+        </div>
+      </div>
+      <div className="p3main">
+        <div className="left3">
+          <div>
+            <h4>
+              1,600 <br />
+              <span>possible mastery points</span>
+            </h4>
+          </div>
+
+          <div>
+            <p>Skill Summary</p>
+            <svg
+              aria-label="0 out of 100 Mastery points"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              aria-valuenow="0"
+              class="_1s2t3cl"
+            //   xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="37"
+              role="progressbar"
+              title="0 out of 100 Mastery points"
+              viewBox="-3 0 20 37"
+            >
+              <rect class="_18qxreg" x="0" y="18" width="14" height="14"></rect>
+              <rect class="_18qxreg" x="0" y="8" width="14" height="8"></rect>
+              <rect class="_qvo65w" x="0" y="30" width="14" height="2"></rect>
+              <polygon
+                class="_18qxreg"
+                points="14 0 10.131 3.774 7 0 3.868 3.775 0 0 0 6 14 6"
+              ></polygon>
+            </svg>
+            <svg
+              aria-label="0 out of 100 Mastery points"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              aria-valuenow="0"
+              class="_1s2t3cl"
+            //   xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="37"
+              role="progressbar"
+              title="0 out of 100 Mastery points"
+              viewBox="-3 0 20 37"
+            >
+              <rect class="_18qxreg" x="0" y="18" width="14" height="14"></rect>
+              <rect class="_18qxreg" x="0" y="8" width="14" height="8"></rect>
+              <rect class="_qvo65w" x="0" y="30" width="14" height="2"></rect>
+              <polygon
+                class="_18qxreg"
+                points="14 0 10.131 3.774 7 0 3.868 3.775 0 0 0 6 14 6"
+              ></polygon>
+            </svg>
+          </div>
+          <div>
+            <p>Counting small numbers</p>
+
+            <svg
+              aria-label="0 out of 100 Mastery points"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              aria-valuenow="0"
+              class="_1s2t3cl"
+            //   xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="37"
+              role="progressbar"
+              title="0 out of 100 Mastery points"
+              viewBox="-3 0 20 37"
+            >
+              <rect class="_18qxreg" x="0" y="18" width="14" height="14"></rect>
+              <rect class="_18qxreg" x="0" y="8" width="14" height="8"></rect>
+              <rect class="_qvo65w" x="0" y="30" width="14" height="2"></rect>
+              <polygon
+                class="_18qxreg"
+                points="14 0 10.131 3.774 7 0 3.868 3.775 0 0 0 6 14 6"
+              ></polygon>
+            </svg>
+            <svg
+              aria-label="0 out of 100 Mastery points"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              aria-valuenow="0"
+              class="_1s2t3cl"
+            //   xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="37"
+              role="progressbar"
+              title="0 out of 100 Mastery points"
+              viewBox="-3 0 20 37"
+            >
+              <rect class="_18qxreg" x="0" y="18" width="14" height="14"></rect>
+              <rect class="_18qxreg" x="0" y="8" width="14" height="8"></rect>
+              <rect class="_qvo65w" x="0" y="30" width="14" height="2"></rect>
+              <polygon
+                class="_18qxreg"
+                points="14 0 10.131 3.774 7 0 3.868 3.775 0 0 0 6 14 6"
+              ></polygon>
+            </svg>
+          </div>
+          <div>
+            <p>Comapring small numbers</p>
+
+            <svg
+              aria-label="0 out of 100 Mastery points"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              aria-valuenow="0"
+              class="_1s2t3cl"
+            //   xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="37"
+              role="progressbar"
+              title="0 out of 100 Mastery points"
+              viewBox="-3 0 20 37"
+            >
+              <rect class="_18qxreg" x="0" y="18" width="14" height="14"></rect>
+              <rect class="_18qxreg" x="0" y="8" width="14" height="8"></rect>
+              <rect class="_qvo65w" x="0" y="30" width="14" height="2"></rect>
+              <polygon
+                class="_18qxreg"
+                points="14 0 10.131 3.774 7 0 3.868 3.775 0 0 0 6 14 6"
+              ></polygon>
+            </svg>
+            <svg
+              aria-label="0 out of 100 Mastery points"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              aria-valuenow="0"
+              class="_1s2t3cl"
+            //   xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="37"
+              role="progressbar"
+              title="0 out of 100 Mastery points"
+              viewBox="-3 0 20 37"
+            >
+              <rect class="_18qxreg" x="0" y="18" width="14" height="14"></rect>
+              <rect class="_18qxreg" x="0" y="8" width="14" height="8"></rect>
+              <rect class="_qvo65w" x="0" y="30" width="14" height="2"></rect>
+              <polygon
+                class="_18qxreg"
+                points="14 0 10.131 3.774 7 0 3.868 3.775 0 0 0 6 14 6"
+              ></polygon>
+            </svg>
+          </div>
+          <div>
+            <p>Quiz 1:5 questions</p>
+            <p>Practice what you have learnt and level up on your skill</p>
+
+            <svg
+              aria-label="0 out of 100 Mastery points"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              aria-valuenow="0"
+              class="_1s2t3cl"
+            //   xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="37"
+              role="progressbar"
+              title="0 out of 100 Mastery points"
+              viewBox="-3 0 20 37"
+            >
+              <rect class="_18qxreg" x="0" y="18" width="14" height="14"></rect>
+              <rect class="_18qxreg" x="0" y="8" width="14" height="8"></rect>
+              <rect class="_qvo65w" x="0" y="30" width="14" height="2"></rect>
+              <polygon
+                class="_18qxreg"
+                points="14 0 10.131 3.774 7 0 3.868 3.775 0 0 0 6 14 6"
+              ></polygon>
+            </svg>
+            <svg
+              aria-label="0 out of 100 Mastery points"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              aria-valuenow="0"
+              class="_1s2t3cl"
+            //   xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="37"
+              role="progressbar"
+              title="0 out of 100 Mastery points"
+              viewBox="-3 0 20 37"
+            >
+              <rect class="_18qxreg" x="0" y="18" width="14" height="14"></rect>
+              <rect class="_18qxreg" x="0" y="8" width="14" height="8"></rect>
+              <rect class="_qvo65w" x="0" y="30" width="14" height="2"></rect>
+              <polygon
+                class="_18qxreg"
+                points="14 0 10.131 3.774 7 0 3.868 3.775 0 0 0 6 14 6"
+              ></polygon>
+            </svg>
+          </div>
+
+          <div>
+            <p>Comapring small numbers</p>
+
+            <svg
+              aria-label="0 out of 100 Mastery points"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              aria-valuenow="0"
+              class="_1s2t3cl"
+            //   xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="37"
+              role="progressbar"
+              title="0 out of 100 Mastery points"
+              viewBox="-3 0 20 37"
+            >
+              <rect class="_18qxreg" x="0" y="18" width="14" height="14"></rect>
+              <rect class="_18qxreg" x="0" y="8" width="14" height="8"></rect>
+              <rect class="_qvo65w" x="0" y="30" width="14" height="2"></rect>
+              <polygon
+                class="_18qxreg"
+                points="14 0 10.131 3.774 7 0 3.868 3.775 0 0 0 6 14 6"
+              ></polygon>
+            </svg>
+            <svg
+              aria-label="0 out of 100 Mastery points"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              aria-valuenow="0"
+              class="_1s2t3cl"
+            //   xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="37"
+              role="progressbar"
+              title="0 out of 100 Mastery points"
+              viewBox="-3 0 20 37"
+            >
+              <rect class="_18qxreg" x="0" y="18" width="14" height="14"></rect>
+              <rect class="_18qxreg" x="0" y="8" width="14" height="8"></rect>
+              <rect class="_qvo65w" x="0" y="30" width="14" height="2"></rect>
+              <polygon
+                class="_18qxreg"
+                points="14 0 10.131 3.774 7 0 3.868 3.775 0 0 0 6 14 6"
+              ></polygon>
+            </svg>
+          </div>
+
+          <div>
+            <p>Unit test results</p>
+            <p>Test your knowledge of all skills in this unit</p>
+
+            <svg
+            //   xmlns="http://www.w3.org/2000/svg"
+            //   xmlns:xlink="http://www.w3.org/1999/xlink"
+              width="106"
+              height="84"
+              viewBox="0 0 75 84"
+            >
+              <defs>
+                <path id="a" d="M0 0h60v26H0z"></path>
+              </defs>
+              <g fill="none" fill-rule="evenodd" transform="translate(-9 -57)">
+                <g transform="translate(12 59)">
+                  <g transform="translate(4)">
+                    <mask id="b" fill="#fff">
+                      {/* <use xlink:href="#a"></use> */}
+                    </mask>
+                    <path
+                      d="M59.98 9.72L56.37 26H3.64L0.03 9.72C-0.17 8.56.67 7.46 1.83 7.27c.58-0.06 1.23.07 1.68.45l11.15 7.24c0.90.78 2.26.65 3.03-0.32.07-0.06.07-.13.13-0.19L28.2.94c0.65-.97 2.00-1.23 2.97-0.58.26.13.45.32.58.58l10.38 13.5c0.64.97 2.00 1.23 2.96.58.06-0.07.13-0.07.19-.13L56.44 7.66c0.90-0.78 2.26-0.65 3.3.32.45.52.58 1.16.52 1.74"
+                      mask="url(#b)"
+                      fill="#f7f8fa"
+                    ></path>
+                  </g>
+                  <path
+                    stroke="#0A2A66"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M63.97 9.69L60.34 26H7.66L4.03 9.69A2.16 2.16 0 0 1 5.08 7.48a2.15 2.15 0 0 1 2.43.20l11.17 7.24a2.15 2.15 0 0 0 3.15-.46L32.22.95a2.15 2.15 0 0 1 3.57 0l10.38 13.50a2.15 2.15 0 0 0 3.15.46l11.17-7.24a2.15 2.15 0 0 1 2.43-0.20 2.16 2.16 0 0 1 1.05 2.21zM61 48s18.93-14.14 1.89-28"
+                  ></path>
+                  <circle
+                    cx="62"
+                    cy="20"
+                    r="2"
+                    fill="#0A2A66"
+                    fill-rule="nonzero"
+                  ></circle>
+                  <path
+                    stroke="#0A2A66"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 48S-9.93 33.86 7.11 20"
+                  ></path>
+                  <circle
+                    cx="7"
+                    cy="20"
+                    r="2"
+                    fill="#0A2A66"
+                    fill-rule="nonzero"
+                  ></circle>
+                  <path
+                    stroke="#0A2A66"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M18 71v10M52 71v10"
+                  ></path>
+                  <path
+                    fill="#00EDB4"
+                    fill-rule="nonzero"
+                    d="M53.66 71H15.34c-1.95 0-3.82-0.79-5.20-2.18A7.49 7.49 0 0 1 8 63.55V31h53v32.55a7.49 7.49 0 0 1-2.15 5.27A7.30 7.30 0 0 1 53.66 71z"
+                  ></path>
+                  <ellipse
+                    cx="25"
+                    cy="38.5"
+                    fill="#0A2A66"
+                    fill-rule="nonzero"
+                    rx="2"
+                    ry="2.5"
+                  ></ellipse>
+                  <ellipse
+                    cx="45"
+                    cy="38.5"
+                    fill="#0A2A66"
+                    fill-rule="nonzero"
+                    rx="2"
+                    ry="2.5"
+                  ></ellipse>
+                  <rect
+                    width="4"
+                    height="5"
+                    x="33"
+                    y="41"
+                    fill="#FFF"
+                    fill-rule="nonzero"
+                    rx="2"
+                  ></rect>
+                </g>
+              </g>
+            </svg>
+          </div>
+        </div>
+        <div className="right3"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Page3;
